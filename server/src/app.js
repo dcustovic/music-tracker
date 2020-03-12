df2a8468891906fa73fe62aca6a637348d969089
Dominik Čustović <domcus44@gmail.com>
2020-03-12 20:57:35 +0100

    setting up registration page

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-
+const bodyParser = require('body-parser')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(cors())
+app.use(bodyParser.json())
 
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: "Hello there from the server"
+        message: `Greetings ${req.body.email} from express.js`
     })
 })
 
