de7ed240b4a7edcc7c84c63098d8a126e805569f
Dominik Čustović <domcus44@gmail.com>
2020-03-16 17:19:27 +0100

    updated registration page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>
-   <h1>Registration</h1>
-   <input type="email" name="email" placeholder="E-mail" v-model="email">
-   <br>
-   <input type="password" name="password" placeholder="Password" v-model="password">
-   <br>
-   <button @click="register">Register</button>
-   <div class="error" v-html="error" />
-  </div>
+  <v-container>
+      <section>
+        <v-col xs="12">
+          <div class="text-center">
+            <h1>Registration</h1>
+          </div>
+        </v-col>
+      </section>
+
+      <v-row class="justify-center">
+        <v-col xs="5" sm="7" md="7" lg="6" xl="4">
+          <v-card-text>
+            <v-form class="px-3" ref="form">
+              <v-text-field
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              v-model="email"
+              >
+              </v-text-field>
+              <br>
+              <v-text-field
+              type="password"
+              name="password"
+              placeholder="Password"
+              v-model="password"
+              >
+              </v-text-field>
+              <br>
+              <div class="errortext" v-html="error" />
+              <br>
+              <v-btn outlined color="primary" @click="register">Register</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-col>
+      </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -37,7 +65,7 @@ export default {
 </script>
 
 <style scoped>
-.error {
+.errortext {
   color: red;
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,20 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+const vuetifyOptions = { }
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  vuetify: new Vuetify(vuetifyOptions),
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  render: h => h(App)
 })
