83d8a2165188a1d49fb84047cf42d19923147e8b
Dominik Čustović <domcus44@gmail.com>
2020-03-20 14:12:08 +0100

    styled footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,7 +1,28 @@
 <template>
-  <v-footer absolute>
-    <v-col class="text-center">
-      GuitarTabs •<strong> Vue</strong>
-    </v-col>
+  <v-footer absolute dark color="grey darken-2">
+    <v-row justify="center" class="mt-3">
+      GuitarTabs • Vue
+    </v-row>
+  <v-col cols="12">
+    <v-divider></v-divider>
+  </v-col>
+  <v-row justify="center">
+    <v-btn icon href="https://www.instagram.com" target="_blank"><v-icon>mdi-instagram</v-icon></v-btn>
+    <v-btn icon href="https://www.facebook.com" target="_blank"><v-icon>mdi-facebook</v-icon></v-btn>
+    <v-btn icon href="https://www.youtube.com" target="_blank"><v-icon>mdi-youtube</v-icon></v-btn>
+  </v-row>
+
   </v-footer>
 </template>
+
+<style scoped>
+.mdi-instagram:hover {
+    color: rgb(177, 118, 245);
+}
+.mdi-facebook:hover {
+    color: rgb(111, 126, 255);
+}
+.mdi-youtube:hover {
+    color: rgb(247, 58, 58);
+}
+</style>
